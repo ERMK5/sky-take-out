1991f434ece5c2ecc2c9e13177fba0c057e41378
ERMK5 <1044437639@qq.com>
2026-07-27 16:23:44 +0800

    用户下单功能业务代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
@@ -0,0 +1,14 @@
+package com.sky.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.ArrayList;
+
+@Mapper
+public interface OrderDetailMapper {
+    /**
+     * 批量插入订单明细
+     * @param orderDetailsList
+     */
+    void insertBatch(ArrayList<Object> orderDetailsList);
+}
